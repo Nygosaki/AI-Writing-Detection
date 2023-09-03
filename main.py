97175545e580cb7a336d3bb483d06bfb0d945cb6
Nygosaki <63132234+Nygosaki@users.noreply.github.com>
2023-09-03 12:53:49 +0200

    Fixed ContentAtScale AI Detector

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # ---------------------------GENERAL CONFIG--------------------------------------
 load_time = 10 # How many seconds does your computer take to load webpages? Increase this if you are getting "unavaible" on most of the tools. YOu can find the exact time by enabling debug mode and timing it with your phone.
-generation_time = 4  # How many seconds to wait for results to generate in tools where we don't have a way of telling when it has finished generating?
+generation_time = 10  # How many seconds to wait for results to generate in tools where we don't have a way of telling when it has finished generating?
 debug_mode = True # Is debug mode enabled?
 headless_mode = False # If enabled, you won't see the browser window. This would allow you to do other things while the text is being proccesed. It breakes some tools (as mentioned in the README)
 
@@ -231,11 +231,12 @@ for i in files_to_check:
                     logger.warning("Could not use Selenium Click ContentAtScale")
                     driver.execute_script('arguments[0].click();', driver.find_element(by=By.XPATH, value='//*[@class="site-btn-secondry-lg check-ai-score"]'))
                     pass
-                contentatscale_result = wait_until_element_text('//*[@class="text-center score-message"]').text
-                if contentatscale_result == "":
+                contentatscale_result_ai_written = wait_until_element_text('//*[@id="progress"]').text
+                if contentatscale_result_ai_written == "":
                     results[f"{i}"]["ContentAtScale AI Detector"] = "unavaible"
                 else:
-                    results[f"{i}"]["ContentAtScale AI Detector"] = contentatscale_result
+                    contentatscale_result = human_to_ai_percentage(contentatscale_result_ai_written)
+                    results[f"{i}"]["ContentAtScale AI Detector"] = f"{contentatscale_result}% probability of AI written"
             except:
                 results[f"{i}"]["ContentAtScale AI Detector"] = "unavaible/error"
                 if debug_mode:
